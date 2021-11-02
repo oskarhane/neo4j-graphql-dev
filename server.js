8f3141a3722d20956693650444fc16bf75876a59
Oskar Hane <oh@oskarhane.com>
2021-11-02 15:28:46 +0100

    Fix issues

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
+var cors = require("cors");
 let server;
 
 module.exports.stopCurrent = async function stopCurrent() {
@@ -16,15 +17,16 @@ module.exports.stopCurrent = async function stopCurrent() {
 };
 
 module.exports.startServer = function startServer(driver, db, typeDefs) {
-    const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+    const neoSchema = new Neo4jGraphQL({ typeDefs, driver, config: { driverConfig: { database: db } } });
     const port = process.env.PORT || 4000;
     const app = express();
+    app.use(cors());
     app.use(
         "/graphql",
         graphqlHTTP({
             schema: neoSchema.schema,
             graphiql: true,
-            context: ({ req }) => ({ req, driver, driverConfig: { database: db } }),
+            context: ({ req }) => ({ req, driver }),
         })
     );
     server = app.listen(port, () => console.log(`GraphiQL ready at http://localhost:${port}/graphql`));
